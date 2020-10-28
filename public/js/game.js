19f498a3e8fc1f8e55a03894fa68edb7f49b7466
Julian Rocha <jules.r.rocha@gmail.com>
2020-10-27 22:00:53 -0700

    Finished off the phaser tutorial to get this lovely game where 2 mushrooms can move and both clients are updated, will need major refactoring but a good start

--- public/js/game.js
+++ public/js/game.js
@@ -19,19 +19,91 @@ var config = {
  
 var game = new Phaser.Game(config);
  
-function preload() {}
+function preload() {
+  this.load.image('mushroom', 'assets/mushroom2.png');
+}
  
 function create() {
   var self = this;
   this.socket = io();
+  this.otherPlayers = this.physics.add.group();
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
       if (players[id].playerId === self.socket.id) {
         addPlayer(self, players[id]);
+      } else {
+        addOtherPlayers(self, players[id]);
       }
     });
   });
+  this.socket.on('newPlayer', function (playerInfo) {
+    addOtherPlayers(self, playerInfo);
+  });
+  this.socket.on('disconnect', function (playerId) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
+    });
+  });
+  this.socket.on('playerMoved', function (playerInfo) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerInfo.playerId === otherPlayer.playerId) {
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
+    });
+  });
+  this.cursors = this.input.keyboard.createCursorKeys();
 }
  
-function update() {}
+function update() {
+  if(this.mushroom) {
+    if(this.cursors.left.isDown) {
+      this.mushroom.setVelocityX(-160);
+    } else if (this.cursors.right.isDown) {
+      this.mushroom.setVelocityX(160);
+    } else if (this.cursors.up.isDown) {
+      this.mushroom.setVelocityY(-160);
+    } else if (this.cursors.down.isDown) {
+      this.mushroom.setVelocityY(160);
+    } else {
+      this.mushroom.setVelocityX(0);
+      this.mushroom.setVelocityY(0);
+    }
+    // emit player movement
+    var x = this.mushroom.x;
+    var y = this.mushroom.y;
+    if (this.mushroom.oldPosition && (x !== this.mushroom.oldPosition.x || y !== this.mushroom.oldPosition.y)) {
+      this.socket.emit('playerMovement', { x: this.mushroom.x, y: this.mushroom.y });
+    }
 
+    // save old position data
+    this.mushroom.oldPosition = {
+      x: this.mushroom.x,
+      y: this.mushroom.y,
+    };
+  }
+}
+
+function addPlayer(self, playerInfo) {
+  self.mushroom = self.physics.add.image(playerInfo.x, playerInfo.y, 'mushroom').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.colour === 'blue') {
+    self.mushroom.setTint(0x0000ff);
+  } else {
+    self.mushroom.setTint(0xff0000);
+  }
+  self.mushroom.setDrag(100);
+  self.mushroom.setAngularDrag(100);
+  self.mushroom.setMaxVelocity(200);
+}
+
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'mushroom').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.colour === 'blue') {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+
 var players = {};
 
 app.use(express.static(__dirname + '/public'));
@@ -31,6 +32,13 @@ io.on('connection', function (socket) {
 		delete players[socket.id];
 		// emit a message to all players to remove this player
 		io.emit('disconnect', socket.id);
+	});
+	// when a player moves, update the player data
+	socket.on('playerMovement', function (movementData) {
+		players[socket.id].x = movementData.x;
+		players[socket.id].y = movementData.y;
+		// emit a message to all players about the player that moved
+		socket.broadcast.emit('playerMoved', players[socket.id]);
 	});
 });
 
