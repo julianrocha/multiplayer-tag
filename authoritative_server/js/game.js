155735f88b03c8558f7f746d30dcc03ff88a348f
chris <chriswest@uvic.ca>
2020-11-28 19:00:10 -0800

    Improved functionality of move states and got rid of redundant code. Also added platform group functionality

--- authoritative_server/js/game.js
+++ authoritative_server/js/game.js
@@ -28,10 +28,11 @@ const config = {
       const self = this;
 
       this.playerPhysGroup = this.physics.add.group();
-      let groundX = this.sys.game.config.width / 2; 
-      let groundY = this.sys.game.config.height * .95;
-      this.ground = this.physics.add.sprite(groundX, groundY, 'ground');
-      this.ground.setImmovable();
+      this.platforms = this.physics.add.group();
+      buildPlatform(self, this.sys.game.config.width / 2, this.sys.game.config.height * 1, 1.5, 0.2);
+      buildPlatform(self, 0, this.sys.game.config.height * 0.66, 0.5, 0.2);
+      buildPlatform(self, this.sys.game.config.width / 1, this.sys.game.config.height * 0.66, 0.5, 0.2);
+      buildPlatform(self, this.sys.game.config.width / 2, this.sys.game.config.height * 0.33, 0.5, 0.2);
       io.on('connection', function(socket){
         console.log('user ' + socket.id + ' connected');
         // create a new player and add it to our playerStates object
@@ -64,7 +65,7 @@ const config = {
         });
         // when a player moves, update the player data
         socket.on('playerInput', function (inputData) {
-        handlePlayerInput(self, socket.id, inputData);
+          handlePlayerInput(self, socket.id, inputData);
         });
       });
   }
@@ -72,35 +73,11 @@ const config = {
   function update() {
     this.playerPhysGroup.getChildren().forEach((player) => {
         const input = playerStates[player.playerId].input;
-        /* console.log("player.playerId" + player.playerId); */
         player.ms = player.ms.goToNextState(player, input);
-        /* if (input.left) {
-          //maybe do an if and else here to see what state to go to next, air lean or sprinting (check by onGround)
-          player.setVelocityX(-160);
-        } else if (input.right) {
-          //maybe do an if and else here to see what state to go to next, air lean or sprinting (check by onGround)
-          player.setVelocityX(160);
-        } else{
-          player.setVelocityX(0);
-        }
-        //Slanted jumping, x velocity and y velocity
-        console.log(player.body.velocity.y);
-        if (input.up && player.onGround === true && player.body.velocity.y === 0) {
-          player.body.setVelocityY(-160);
-          player.onGround = false;
-        } else {
-          player.setGravityY(300);
-        } */
         playerStates[player.playerId].x = player.x;
         playerStates[player.playerId].y = player.y;
-        playerStates[player.playerId].onGround = player.onGround;
       });
-      this.physics.world.wrap(this.playerPhysGroup, 5);
       io.emit('playerUpdates', playerStates);
-  }
-
-  function onGroundFunc(player){
-    player.onGround = true;
   }
 
   function handlePlayerInput(self, playerId, input) {
@@ -111,30 +88,42 @@ const config = {
     });
   }
 
+  function buildPlatform(self, xLoc, yLoc, xScale, yScale){
+    const platform = self.physics.add.sprite(xLoc, yLoc, 'ground');
+    platform.xLoc = xLoc;
+    platform.yLoc = yLoc;
+    platform.scaleX = xScale;
+    platform.scaleY = yScale;
+    self.physics.add.collider(platform, self.playerPhysGroup);
+    self.platforms.add(platform);
+    platform.setImmovable();
+  }
+
   function addPlayer(self, playerInfo){
     const player = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'mushroom').setOrigin(0, 0).setDisplaySize(53, 40, true);
-    /* player.setDrag(100);
-    player.setAngularDrag(100); */
-    player.setMaxVelocity(200);
-    /* player.setGravityY(300); */
+    player.setMaxVelocity(500);
     player.body.enable;
     player.playerId = playerInfo.playerId;
-    player.onGround = false;
     player.gameInstance = self;
-    /* player.body.collideWorldBounds = true; */
     if(Object.keys(playerStates).length == 1){
       player.ts = new Tagged(player);
     } else {
       player.ts = new NotTagged(player);
     }
     player.ms = new inAir(player);
-    self.physics.add.collider(player, self.ground, onGroundFunc);
     self.playerPhysGroup.getChildren().forEach((otherPlayer) => {
       self.physics.add.overlap(player, otherPlayer, function (player, otherPlayer) {
         handlePlayerCollision(player, otherPlayer);
       });
     });
     self.playerPhysGroup.add(player);
+    player.setCollideWorldBounds(true);
+    player.setGravityY(300);
+    player.setDrag(25);
+  }
+
+  function collidedFunc(player){
+    console.log("collided");
   }
 
   function removePlayer(self, playerId) {
@@ -151,26 +140,18 @@ const config = {
 
   class airLean {
     constructor(p){
-      playerStates[p.playerId].colour = 0x00FF00;
+      console.log("ENTERED THE AIRLEAN STATE");
     }
 
     goToNextState(player, input){
-      /* console.log("entered go to next state in airlean"); */
-      console.log(player.body.velocity.y);
       /* If the player is on the ground and the one of the right or left inputs are being pressed, go to sprinting state */
-      if(player.onGround === true && (input.left || input.right)){
-        console.log(player.onGround);
-        player.onGround = false;
-        player.ms = new Sprinting(player);
-        console.log("going to sprinting from airLean");
-        return player.ms;
+      if(player.body.touching.down && (input.left || input.right)){
+        return new Sprinting(player);
       } 
       /* If neither input left or right are being pressed and the player is stil in the air, then go to the inAir state */
       else if(!(input.left || input.right)){
-        player.setVelocityX(0);
-        player.ms = new inAir(player);
-        console.log("going to inAir from airLean");
-        return player.ms;
+        player.setAccelerationX(0);
+        return new inAir(player);
       } else{
         return this;
       }
@@ -179,28 +160,21 @@ const config = {
 
   class inAir {
     constructor(p){
-      playerStates[p.playerId].colour = 0x0000FF;
+      console.log("ENTERED THE INAIR STATE");
     }
     goToNextState(player, input){
-      player.setGravityY(300);
-      /* console.log("entered gotonextstate in inair"); */
       /* Go to the air lean state if either input left or right exists. */
       if(input.left || input.right) {
         if(input.left){
-          player.setVelocityX(-500);
+          player.setAccelerationX(-350);
         } else{
-          player.setVelocityX(500);
+          player.setAccelerationX(350);
         }
-        /* console.log("going to airLean from inair"); */
-        player.ms = new airLean(player);
-        return player.ms;
+        return new airLean(player);
       } 
-      /* Go to standing state if all directional velocitys are 0 and the player is on the ground */
-      else if(player.body.velocity.y === 0 && player.onGround === true && player.body.velocity.x === 0){
-        console.log("going to standing from inair");
-        console.log(player.onGround);
-        player.ms = new Standing(player);
-        return player.ms;
+      /* Go to standing state if the player is on the ground */
+      else if(player.body.touching.down){
+        return new Standing(player);
       } else{
         return this;
       }
@@ -209,30 +183,23 @@ const config = {
 
   class Sprinting {
     constructor(p){
-      playerStates[p.playerId].colour = 0xFFFF00;
+      console.log("ENTERED THE SPRINTING STATE");
     }
 
     goToNextState(player, input){
       /* Going to air lean with a jump */
-      if(input.up && player.onGround === true && player.body.velocity.y === 0 && (input.left || input.right)){
-        player.setVelocityY(-500);
-        player.ms = new airLean(player);
-        player.onGround = false;
-        /* console.log("going to airlean from sprinting"); */
-        return player.ms;
+      if(input.up && player.body.touching.down && (input.left || input.right)){
+        player.setVelocityY(-400);
+        return new airLean(player);
       } 
       /* Going to air lean from falling off a ledge */
-      else if((input.left || input.right) && player.onGround === true && player.body.velocity.y !== 0){
-        player.ms = new airLean(player);
-        player.onGround = false;
-        return player.ms;
+      else if((input.left || input.right) && !player.body.touching.down){
+        return new airLean(player);
       } 
       /* Going to standing state when no inputs are being pressed and the player is touching the ground */
-      else if(!(input.left || input.right || input.up) && player.onGround === true){
-        player.setVelocityX(0);
-        player.ms = new Standing(player);
-        /* console.log("going to standing from sprinting"); */
-        return player.ms;
+      else if(!(input.left || input.right || input.up) && player.body.touching.down){
+        player.setAccelerationX(0);
+        return new Standing(player);
       } 
       else{
         return this;
@@ -242,30 +209,23 @@ const config = {
 
   class Standing {
     constructor(p){
-      playerStates[p.playerId].colour = 0xF0000F;
+      console.log("ENTERED THE STANDING STATE");
     }
 
     goToNextState(player, input){
-      player.setGravityY(300);
-      /* console.log("entered go to next state in Standing"); */
       /* If input left or right are being pressed and input up is not being pressed, then go to the sprinting state */
       if((input.left || input.right) && !input.up) {
         if(input.left){
-          player.setVelocityX(-160);
+          player.setAccelerationX(-350);
         } else{
-          player.setVelocityX(160);
+          player.setAccelerationX(350);
         }
-        /* console.log("going to sprinting from standing"); */
-        player.ms = new Sprinting(player);
-        return player.ms;
-      } 
+        return new Sprinting(player);
+      }
       /* if input up is being pressed and all directional velocitys are zero, then go to the inAir state */
-      else if(input.up && player.body.velocity.y === 0 && player.body.velocity.x === 0){
-        player.setVelocityY(-500);
-        player.ms = new inAir(player);
-        player.onGround = false;
-        /* console.log("going to inAir from standing"); */
-        return player.ms;
+      else if(input.up){
+        player.setVelocityY(-400);
+        return new inAir(player);
       } else{
         return this;
       }
--- public/js/game.js
+++ public/js/game.js
@@ -28,11 +28,27 @@ var playerStates = {};
 function create() {
   var self = this;
   this.socket = io();
-  this.players = this.physics.add.group();
-
+  this.players = this.add.group();
   let groundX = this.sys.game.config.width / 2; 
-  let groundY = this.sys.game.config.height * .95;
+  let groundY = this.sys.game.config.height * 1;
   this.ground = this.add.sprite(groundX, groundY, 'ground');
+  this.ground.scaleX = 1.5;
+  this.ground.scaleY = 0.2;
+  let gX = 0; 
+  let gY = this.sys.game.config.height * .66;
+  this.smallLeftGround = this.add.sprite(gX, gY, 'ground');
+  this.smallLeftGround.scaleX = 0.5;
+  this.smallLeftGround.scaleY = 0.2;
+  let g2X = this.sys.game.config.width / 1; 
+  let g2Y = this.sys.game.config.height * .66;
+  this.smallRightGround = this.add.sprite(g2X, g2Y, 'ground');
+  this.smallRightGround.scaleX = 0.5;
+  this.smallRightGround.scaleY = 0.2;
+  let g3X = this.sys.game.config.width / 2; 
+  let g3Y = this.sys.game.config.height * .33;
+  this.smallTopGround = this.add.sprite(g3X, g3Y, 'ground');
+  this.smallTopGround.scaleX = 0.5;
+  this.smallTopGround.scaleY = 0.2;
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
       displayPlayers(self, players[id], 'mushroom');
